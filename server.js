5d96a6b8fb9f1a1d71357c3e8bf59a4155d3da8a
Joey Pinhas <jpinhas97@yahoo.com>
2016-10-27 11:26:10 -0400

    Simple input form gets shown on submit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
 const port = process.argv[2] || process.env.PORT || 3000;
@@ -8,12 +11,26 @@ const port = process.argv[2] || process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
+// middleware to receive form inputs
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// middleware for method override
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // routes
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/show', (req, res) => {
+  res.json(req.body);
+});
+
+app.get('/show', (req, res) => {
+  res.json(req.query);
 });
 
 
