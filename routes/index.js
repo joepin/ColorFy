b1b6ad8fe3318a00f4679a98278776ae4ac1ab41
Joey Pinhas <jpinhas97@yahoo.com>
2016-10-31 13:40:31 -0400

    Lots of changes - begins styling, and unsuccessfully attempts to build a word cloud

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 const router = require('express').Router();
 const watsonService = require('../services/watson');
 const recents = require('../models/recents');
+const userModel = require('../models/user');
 const functions = require('../lib/functions');
 const auth = require('../lib/auth');
 
 router.get('/', recents.getAllRecents, recents.getAllWordFrequencies, (req, res) => {
+  const userObj = userModel.getUserById(req.session.userID)
   res.render('home/index', {
+    user: userObj.username,
     recents: res.allRecents,
     frequencies: res.allFrequencies,
   });
-  // res.json(res.allRecents);
+  // res.json(res.session.user);
 });
 
 router.get('/search', (req, res) => {
   res.render('home/search');
 })
 
-router.post('/show', auth.authenticate, watsonService.analyzeText, functions.getColors, recents.addToRecents, functions.getWordCount, recents.addToWordFrequency, (req, res) => {
+router.post('/show', watsonService.analyzeText, functions.getColors, recents.addToRecents, functions.getWordCount, recents.addToWordFrequency, (req, res) => {
   // res.json(res.data);
   res.render('home/results', {
     colors: JSON.stringify(res.colors),
@@ -30,6 +33,10 @@ router.post('/show', auth.authenticate, watsonService.analyzeText, functions.get
     tones2: res.data.document_tone.tone_categories[2].tones,
   });
 });
+
+router.get('/test', (req, res) => {
+  res.json(req.session);
+})
 
 router.get('/login', (req, res) => {
   res.render('home/login');
